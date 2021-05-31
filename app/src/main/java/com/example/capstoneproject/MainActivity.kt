94cf21b33be580bfb8aed0cb5f20d83abf5ad760
DanielAnyien <ppprocash@gmail.com>
2021-05-31 21:36:34 +0700

    Tambahan integrasi app dengan model

--- app/src/main/java/com/example/capstoneproject/MainActivity.kt
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.example.capstoneproject
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ImageButton
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.capstoneproject.adapter.DataAdapter
+import com.example.capstoneproject.ml.Model
 import com.example.capstoneproject.model.DataItem
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var dataBase: DatabaseReference
     private lateinit var dtList: ArrayList<DataItem>
     private lateinit var dtAdapter:DataAdapter
-    private val cameraRequest = 1888
+    lateinit var result: TextView
+    lateinit var diagnosa: Button
+    lateinit var select: Button
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +42,46 @@ class MainActivity : AppCompatActivity() {
         rv_penyakit.setHasFixedSize(true)
         getItemData()
 
-        val photoButton: ImageButton = findViewById(R.id.btnfoto)
-        photoButton.setOnClickListener {
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(cameraIntent, cameraRequest)
-        }
+        imageView = findViewById(R.id.gambarpenyakit)
+        diagnosa = findViewById(R.id.btndiagnosa)
+        select = findViewById(R.id.btnselect)
+        result = findViewById(R.id.tv_result)
+
+        val label = application.assets.open("labels.txt").bufferedReader()
+                .use { it.readText() }.split("\n")
+
+        select.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            startActivityForResult(intent, 100)
+        })
+
+        diagnosa.setOnClickListener(View.OnClickListener {
+            val resize: Bitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, true)
+            val model = Model.newInstance(this)
+            val buffer = TensorImage.fromBitmap(resize)
+            val byteBuffer = buffer.buffer
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 100, 100, 3), DataType.FLOAT32)
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+            var max = getMax(outputFeature0.floatArray)
+
+            result.setText(label[max])
+
+            model.close()
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        imageView.setImageURI(data?.data)
+        var uri: Uri ?= data?.data
+        bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,6 +99,19 @@ class MainActivity : AppCompatActivity() {
             }
             else -> return true
         }
+    }
+
+    fun getMax(array: FloatArray) : Int{
+        var index = 0
+        var min = 0.0f
+
+        for (n in 0..4){
+            if (min < array[n]){
+                index = n
+                min = array[n]
+            }
+        }
+        return index
     }
 
     private fun getItemData() {
